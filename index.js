13f1c33b60f07390b60c8f2cd9e78d4f80cce0f4
RafaelCardoso990 <rafaelcardoso990@gmail.com>
2022-04-29 12:40:38 -0300

    post message implementado

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-import express from 'express'
+import express, {json} from 'express'
 import chalk from 'chalk';
 import cors from 'cors'
 import Joi from 'joi';
 import { MongoClient } from 'mongodb';
+import dayjs from 'dayjs';
 
 const app = express();
-app.use(express.json());
+app.use(json());
 app.use(cors());
 
+let user = null
+let now = dayjs().format("hh:mm:ss")
+
 let database = null;
-const mongoClient = new MongoClient("mongodb://127.0.0.1:27017");
+const mongoClient = new MongoClient("mongodb://localhost:27017");
 const promise = mongoClient.connect()
 
-promise.then(response => {
+promise.then(() => {
     database = mongoClient.db("bate-papo-uol")
     console.log(chalk.bold.yellowBright("Banco de dados conectado"))
 })
@@ -21,14 +25,12 @@ promise.catch(e => console.log(chalk.red("Xabu"), e));
 
 
 const schema = Joi.object().keys({
-name: Joi.string().min(3)
+    name: Joi.string().min(3)
 });
 
-app.post('/participants', (req, res) => {
-
+app.post("/participants", async (req, res) => {
     
-    const {name} = req.body
-    
+    const {name} = req.body    
     
     const {error, value} = schema.validate({name});
     
@@ -39,19 +41,61 @@ app.post('/participants', (req, res) => {
     
     const participants = {name, lastStatus: Date.now()}
 
-  
-    const promise = database.collection('participants').insertOne(participants)
-    promise.then(confirmação => {
-        console.log(confirmação);
+    try{  
+    
+        const participantes = await database.collection("participants").insertOne(participants)
         res.sendStatus(201)
-    })
-    promise.catch(e => {
+    
+    } catch(e) {
+        
         console.log(chalk.pink("deu ruim"), e);
         res.status(500)
-    })
+
+    }  
 
 })
 
+app.get("/participants", async (req, res) => {
+    user = req.headers.user
+    try {
+
+        const participantes = await database.collection("participants").find({}).toArray();
+        res.send(participantes)    
+
+    } catch(e){
+
+        console.log(chalk.bold.pink("Deu ruim"), e)
+        res.sendStatus(500)
+    }
+
+})
+
+app.post("/messages", async (req, res) => {        
+    const {to, text, type} = req.body
+    const message = {
+        from: user,
+        to: to,
+        text: text,
+        type: type,
+        time: now
+    }
+
+    console.log(message)
+
+    try{  
+    
+        const messages = await database.collection("message").insertOne(message)
+        res.sendStatus(201)
+    
+    } catch(e) {
+        
+        console.log(chalk.pink("deu ruim"), e);
+        res.status(500)
+
+    }  
+
+
+})
 
 app.listen(5000 ,() =>{
 console.log(chalk.bold.blue('Server listening on port 5000'));
